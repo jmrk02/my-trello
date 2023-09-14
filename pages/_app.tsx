1db8fb8910f4bb3d5db7c45d94da0c4f61ffbf3b
jmrk02 <jeanmarcoe2@gmail.com>
2023-09-14 11:14:30 -0500

    finish my trello

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { UIProvider } from "../context/ui";
+import { EntriesProvider } from "../context/entries";
+import { SnackbarProvider } from "notistack";
+
+import { lightTheme, darkTheme } from "../themes";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <EntriesProvider>
+        <UIProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UIProvider>
+      </EntriesProvider>
+    </SnackbarProvider>
+  );
 }
+
+export default MyApp;
